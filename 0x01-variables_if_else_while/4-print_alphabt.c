1cb066e1d2518b9d6ef877198217ca4366e616ce
shaaareeef <sherifkahraba500@gmail.com@gmail.com>
2023-07-14 19:10:15 -0700

    a

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main (void)
+{
+        int loweCase = 'a';
+        while (lowerCase <= 'z')
+        {
+		if (lowerCase == 'e' || lowerCase == 'q')
+		{
+			lowerCase += 1;
+		}
+		else
+		{
+                putchar(lowrCase);
+                lowerCase += 1;
+		}
+        }
+        putchar('\n');
+        return (0);
+}
