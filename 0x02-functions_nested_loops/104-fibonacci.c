eb0a65728fa08232f271dbbc339aedbfcaf55d41
shaaareeef <sherifkahraba500@gmail.com@gmail.com>
2023-07-19 10:15:51 -0700

    a

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -37,7 +37,7 @@ int main(void)
 	int count, initial0s;
 	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
 
-	for (count = 1; count < 98; count++)
+	for (count = 1; count <= 98; count++)
 	{
 		if (f1o > 0)
 			printf("%lu", f1o);
@@ -61,7 +61,7 @@ int main(void)
 		if (count != 98)
 			printf(", ");
 		else
-		printf("\n");
+			printf("\n");
 	}
 	return (0);
 }
