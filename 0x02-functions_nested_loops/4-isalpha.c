2a0d5b0a1a41e086cb0ee24bd03d4bfad48bc050
shaaareeef <sherifkahraba500@gmail.com@gmail.com>
2023-07-19 07:31:56 -0700

    a

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (sucess)
+*/
+
+int main(void)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
