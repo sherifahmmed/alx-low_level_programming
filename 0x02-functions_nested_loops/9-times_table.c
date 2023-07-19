49dc0d4312ddbf064578f0f7c2237df14212f701
shaaareeef <sherifkahraba500@gmail.com@gmail.com>
2023-07-19 09:00:45 -0700

    a

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,7 +17,7 @@ void times_table(void)
 		_putchar(48);
 		for (mult = 0; mult <= 9; mult++)
 		{
-			_putcjar(',');
+			_putchar(',');
 			_putchar(' ');
 
 			prod = num * mult;
@@ -31,7 +31,7 @@ void times_table(void)
 			else
 				_putchar((prod / 10) + 48); /*get the first digit*/
 
-			_putchar((ptod % 10) + 48); /*get the second digit*/
+			_putchar((prod % 10) + 48); /*get the second digit*/
 		}
 		_putchar('\n');
 	}
