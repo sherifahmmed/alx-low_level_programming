de72dd6587452e9fbb674d2243b24a2bbad9b781
shaaareeef <sherifkahraba500@gmail.com@gmail.com>
2023-07-30 02:23:29 +0000

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle\n")
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+*/
+
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle\n");
+return 0;
+}
