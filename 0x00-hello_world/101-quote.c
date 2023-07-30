519ed4406715869b4bd7830b922450f6d1ba69f9
shaaareeef <sherifkahraba500@gmail.com@gmail.com>
2023-07-30 03:02:54 +0000

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -7,6 +7,7 @@
 
 int main(void)
 {
-putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+write(1, quo, 59);
 return (1);
 }
