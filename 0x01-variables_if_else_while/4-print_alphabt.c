cbd399cd0146716379aa3476810901624b83acb6
shaaareeef <sherifkahraba500@gmail.com@gmail.com>
2023-07-30 18:08:45 +0000

    a

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	int i;
 
-	if (i != 101 && i != 113)
+	for (i = 97; i <= 122; i++)
 	{
-		for (i = 97; i <= 122; i++)
+		if (i != 101 && i != 113)
 			putchar(i);
 	}
 	putchar('\n');
