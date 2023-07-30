5eac9f0fe79c0686b9b8679bcecde1423cb621c2
shaaareeef <sherifkahraba500@gmail.com@gmail.com>
2023-07-30 02:26:01 +0000

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 /**
  * main - Entry point
- * Return: 0
+ * Return: Always 0 (success)
 */
 
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
-return 0;
+puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
