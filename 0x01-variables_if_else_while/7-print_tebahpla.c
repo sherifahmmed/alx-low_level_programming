60a11eb36b31a9c311b9306eb17e91d4028e801c
shaaareeef <sherifkahraba500@gmail.com@gmail.com>
2023-07-30 18:19:35 +0000

    a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints the alphabet in lowercase.
+ * Return: 0.
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 122; i <= 97; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
