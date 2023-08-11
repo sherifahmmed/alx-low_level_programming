d075f0d2c23da2112c80080892005b013d025f72
shaaareeef <sherifkahraba500@gmail.com@gmail.com>
2023-08-10 19:36:30 -0700

    a

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -12,5 +12,8 @@ int len(char *str);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void _puts(char *str);
+int _atoi(const char *s);
+void print_int(unsigned long int n);
 
 #endif /*MAIN_H*/
