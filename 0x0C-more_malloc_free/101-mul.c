d95b1d19673bdcab179889f7b40601256e7a041b
shaaareeef <sherifkahraba500@gmail.com@gmail.com>
2023-08-10 19:30:40 -0700

    a

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -8,12 +8,13 @@
 
 void _puts(char *str)
 {
-        int i = 0;
-        while (str[i])
-        {
-                _putchar(str[i]);
-                i++;
-        }
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
 }
 
 /**
@@ -24,15 +25,15 @@ void _puts(char *str)
 
 int _atoi(const char *s)
 {
-        int sign = 1;
-        unsigned long int resp = 0, firstNum, i;
+	int sign = 1;
+	unsigned long int resp = 0, firstNum, i;
 
-        for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
-        {
-                if (s[firstNum] == '-')
-                {
-                        sign *= -1;
-                }
+	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
+	{
+		if (s[firstNum] == '-')
+		{
+			sign *= -1;
+		}
 	}
 
 	for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
@@ -52,7 +53,7 @@ int _atoi(const char *s)
 
 void print_int(unsigned long int n)
 {
-	unsigned long int divisor = 1, i ,resp;
+	unsigned long int divisor = 1, i, resp;
 
 	for (i = 0; n / divisor > 9; i++, divisor *= 10)
 	;
@@ -71,14 +72,17 @@ void print_int(unsigned long int n)
  * return: 0
  */
 
-int main(int argc, char constant*argv[])
-{       
-        (void)argc;
-        
-        if (argc != 3)
-        {       
-                _puts("Error ");
-                exit(98);
-        }       
-        print_int(_atoi(argv[1]) * _atoi(argv[2]));
-        _putchar('\n');
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+
+	if (argc != 3)
+	{
+		_puts("Error ");
+		exit(98);
+	}
+	print_int(_atoi(argv[1]) * _atoi(argv[2]));
+	_putchar('\n');
+
+	return (0);
+}
