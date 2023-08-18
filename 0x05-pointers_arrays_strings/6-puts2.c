9eb2dbed6c337a54a3addfc5cb62cefb9c634306
shaaareeef <sherifkahraba500@gmail.com@gmail.com>
2023-08-18 10:08:25 -0700

    a

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,11 +10,10 @@
 
 void puts2(char *str)
 {
-	int i, j = 0;
-
-	while (str[j] != '\0')
-		j++;
-	for (i = 0; i < j; i += 2)
-		_putchar(str[i]);
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str += 2;
+	}
 	_putchar('\n');
 }
