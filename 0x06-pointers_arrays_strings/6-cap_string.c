91319fa3c41a34dcf97144ed20267d0d6758562b
shaaareeef <sherifkahraba500@gmail.com@gmail.com>
2023-08-19 03:27:44 -0700

    a

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,7 @@ char *cap_string(char *str)
 
 	while (str[index] != '\0')
 	{
-		while (str[index] >= 'A' && str[index] <= 'Z')
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
 		if (str[index - 1] == ' ' ||
 		str[index - 1] == '\t' ||
